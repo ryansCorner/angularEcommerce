4fafc9a31ccf6dafa271e0d5e2b441220a77739c
Ryan Felter <42056419+ryansCorner@users.noreply.github.com>
2019-12-03 23:41:10 -0800

    first commit for my angular ecommerce...styles needed

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+    products = products;
+
+    onNotify() {
+        window.alert('YO this Phone is over $700')
+    }
+
+    share() {
+        window.alert('The product has been shared!');
+    }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
